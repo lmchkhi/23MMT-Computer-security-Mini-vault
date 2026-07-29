be5f2b46750c85a3e55263add02959d233ef7c6d
nggfett <nnhphat23@clc.fitus.edu.vn>
2026-07-29 19:43:20 +0700

    Task 2.1 Named Key Management

--- MiniVault_Group/src/storage/models.py
+++ MiniVault_Group/src/storage/models.py
@@ -17,3 +17,15 @@ class KVSecret(db.Model):
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
     
+class NamedKey(db.Model):
+    __tablename__ = 'named_keys'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    key_name = db.Column(db.String(255), unique=True, nullable=False)
+    owner_email = db.Column(db.String(255), nullable=False)
+    key_usage = db.Column(db.String(50), nullable=False, default="ENCRYPT_DECRYPT")
+    
+    # Chuỗi Base64 chứa nonce + ciphertext + tag của khóa AES đã được mã hóa bằng DEK
+    encrypted_key_material_b64 = db.Column(db.Text, nullable=False)
+    
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
